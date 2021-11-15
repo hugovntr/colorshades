980b7e408d0a674bbb0d67accfe1bff6ab292da3
hugovntr <26794193+hugovntr@users.noreply.github.com>
2021-11-15 17:20:00 +0100

    feat (app): Better combinations pages

--- packages/app/src/pages/combinations.tsx
+++ packages/app/src/pages/combinations.tsx
@@ -1,15 +1,17 @@
-import { BeakerIcon } from "@/components/icons";
+import { BeakerIcon, DuplicateIcon } from "@/components/icons";
+import { ColorToast } from "@/components/toasts";
 import useCombinations from "@/hooks/useCombinations";
 import useShades from "@/hooks/useShades";
 import App from "@/layouts/app";
-import { CombinationType } from "colorshades";
-import { FormEvent, useEffect } from "react";
+import { Color, CombinationType, Shades } from "colorshades";
+import React, { FormEvent, MouseEvent, useRef, useState } from "react";
 
 export default function CombinationsPage(): JSX.Element {
     return (
         <App title="Combinations | ColorShades">
             <Title />
             <GetStarted />
+            <Showcase />
         </App>
     );
 }
@@ -45,7 +47,7 @@ function Title(): JSX.Element {
 
 function GetStarted(): JSX.Element {
     const { setColor, color } = useShades();
-    const { setType } = useCombinations();
+    const { setType, type: combinationsType } = useCombinations();
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
@@ -56,22 +58,20 @@ function GetStarted(): JSX.Element {
     return (
         <section
             id="get-started"
-            className="relative bg-strong dark:bg-background"
+            className="relative bg-strong dark:bg-background pb-8"
         >
             <div className="flex items-center">
                 <div className="container max-w-4xl rounded-3xl py-8 space-y-16">
                     <h2 className="text-center dark">Prepare your mixture</h2>
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                        <div>
-                            <p className="">
-                                Input your{" "}
-                                <span className="text-strong dark">
-                                    hexadecimal
-                                </span>{" "}
-                                color, and let the algorithm mix a unique set of
-                                shades for you
-                            </p>
-                        </div>
+                        <p>
+                            Input your{" "}
+                            <span className="text-strong dark">
+                                hexadecimal
+                            </span>{" "}
+                            color...
+                        </p>
+
                         <form
                             onSubmit={handleSubmit}
                             className="flex flex-col items-end space-y-6 dark"
@@ -94,7 +94,12 @@ function GetStarted(): JSX.Element {
                                 </button>
                             </div>
                         </form>
-                        <div className="flex gap-3">
+                        <p className="">
+                            Select the{" "}
+                            <span className="text-strong dark">type</span> of
+                            combinations that you want...
+                        </p>
+                        <div className="flex flex-wrap gap-3">
                             {[
                                 "opposite",
                                 "adjacent",
@@ -106,15 +111,169 @@ function GetStarted(): JSX.Element {
                                         setType(type as CombinationType)
                                     }
                                     key={type}
-                                    className="button dark bg-page text-foreground capitalize text-base"
+                                    className="button dark bg-page text-foreground capitalize"
+                                    disabled={type === combinationsType}
                                 >
                                     {type}
                                 </button>
                             ))}
                         </div>
+                        <div className="sm:col-span-2 flex flex-col items-center pt-8 space-y-2">
+                            <p>And take a look at your final mix</p>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                className="w-8 h-8 animate-bounce text-primary-500"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M16 10L12.7071 13.2929C12.3166 13.6834 11.6834 13.6834 11.2929 13.2929L8 10"
+                                />
+                            </svg>
+                        </div>
                     </div>
                 </div>
             </div>
+        </section>
+    );
+}
+
+function Showcase(): JSX.Element {
+    const { combinations } = useCombinations();
+    const [inspected, inspect] = useState<Color>(null);
+    const copyToClipboardRef = useRef<HTMLButtonElement>(null);
+
+    /**
+     * A very ugly way to copy and alert the user
+     *
+     * @param e - Click event
+     */
+    const copyToClipboard = (
+        e: MouseEvent<HTMLButtonElement>,
+        shades: Shades
+    ) => {
+        e.preventDefault();
+        const target = e.currentTarget;
+        navigator.clipboard
+            .writeText(JSON.stringify(shades))
+            .then(() => {
+                const html = copyToClipboardRef.current.innerHTML;
+                target.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" class="w-6 h-6 text-green-500">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 12l5 4 7-8" />
+                    </svg>
+                    <span>Copied</span>
+                `;
+                setTimeout(() => {
+                    target.innerHTML = html;
+                }, 1000);
+            })
+            .catch(console.error);
+    };
+
+    return (
+        <section id="showcase" className="container">
+            <div className="space-y-16">
+                {combinations.shades.map((shades, index) => {
+                    const mainColor: Color = combinations.colors[index];
+                    return (
+                        <div
+                            className="grid grid-cols-1 lg:grid-cols-3 items-start gap-16"
+                            key={index}
+                        >
+                            <div className="max-w-xl mx-auto flex flex-col items-center lg:items-start">
+                                <h3 className="capitalize">{shades.name}</h3>
+                                <div className="w-full">
+                                    <figure
+                                        className="w-3/4 h-4 rounded transition-colors duration-300"
+                                        style={{
+                                            backgroundColor: mainColor.rgb,
+                                        }}
+                                    ></figure>
+                                    <p className="text-strong text-lg uppercase mb-4 mt-2 select-all">
+                                        {mainColor.hex}
+                                    </p>
+                                    <div className="grid grid-cols-2 gap-6">
+                                        <div>
+                                            <p className="text-smooth text-sm">
+                                                HUE
+                                            </p>
+                                            <p>{mainColor.hue}</p>
+                                        </div>
+                                        <div>
+                                            <p className="text-smooth text-sm">
+                                                SATURATION
+                                            </p>
+                                            <p>{mainColor.saturation * 100}%</p>
+                                        </div>
+                                        <div>
+                                            <p className="text-smooth text-sm">
+                                                LUMINANCE
+                                            </p>
+                                            <p>{mainColor.luminance * 100}%</p>
+                                        </div>
+                                        <div>
+                                            <p className="text-smooth text-sm">
+                                                BRIGHTNESS
+                                            </p>
+                                            <p>{mainColor.brightness}</p>
+                                        </div>
+                                        <div>
+                                            <p className="text-smooth text-sm">
+                                                RGB
+                                            </p>
+                                            <p className="truncate select-all">
+                                                {mainColor.rgb}
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <p className="text-smooth text-sm">
+                                                HSL
+                                            </p>
+                                            <p className="truncate select-all">
+                                                {mainColor.hsl}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button
+                                    ref={copyToClipboardRef}
+                                    onClick={(e) => copyToClipboard(e, shades)}
+                                    className="button mt-8"
+                                >
+                                    <DuplicateIcon className="w-6 h-6" />
+                                    <span>Copy to clipboard</span>
+                                </button>
+                            </div>
+                            <div className="grid grid-cols-2 sm:grid-cols-5 gap-x-4 gap-y-6 lg:col-span-2 bg-background rounded-3xl p-6">
+                                {shades.colors.map((color) => (
+                                    <div
+                                        className="text-sm space-y-1"
+                                        key={color.hex}
+                                    >
+                                        <figure
+                                            onClick={() => inspect(color)}
+                                            className={`w-full h-16 rounded-lg transition-colors duration-300 cursor-pointer`}
+                                            style={{
+                                                backgroundColor: color.rgb,
+                                            }}
+                                        ></figure>
+                                        <p className="uppercase">{color.hex}</p>
+                                        <p className="text-strong">
+                                            {color.index}
+                                        </p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <ColorToast color={inspected} />
         </section>
     );
 }
