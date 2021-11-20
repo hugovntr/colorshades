bfc9cfa2ef25e9df84060101c8773ac1c1b60733
hugovntr <26794193+hugovntr@users.noreply.github.com>
2021-11-20 17:27:53 +0100

    feat (app): Combinations page

--- packages/app/src/pages/combinations.tsx
+++ packages/app/src/pages/combinations.tsx
@@ -1,10 +1,43 @@
-import { BeakerIcon, DuplicateIcon } from "@/components/icons";
+import {
+    AdjacentColorsIcon,
+    BeakerIcon,
+    DuplicateIcon,
+    OppositeColorsIcon,
+    TetradicColorsIcon,
+    TriadicColorsIcon,
+} from "@/components/icons";
 import { ColorToast } from "@/components/toasts";
 import useCombinations from "@/hooks/useCombinations";
 import useShades from "@/hooks/useShades";
 import App from "@/layouts/app";
 import { Color, CombinationType, Shades } from "colorshades";
 import React, { FormEvent, MouseEvent, useRef, useState } from "react";
+
+const availableCombinations = [
+    {
+        type: "opposite",
+        description:
+            "The opposite combination adds 180° of rotation to the input hue",
+        Icon: (props: any) => <OppositeColorsIcon {...props} />,
+    },
+    {
+        type: "adjacent",
+        description:
+            "The adjacent combinations takes 2 colors from the input color, a warmer and a colder. Both of these colors have the same hue distance from the input color's hue",
+        Icon: (props: any) => <AdjacentColorsIcon {...props} />,
+    },
+    {
+        type: "triadic",
+        description:
+            "The triadic combinations, similar to the adjacent, takes 2 colors from the input. The only difference is that all colors have the exact same hue distance between them.",
+        Icon: (props: any) => <TriadicColorsIcon {...props} />,
+    },
+    {
+        type: "tetradic",
+        description: "The tetradic combinations form a rectangle of colors.",
+        Icon: (props: any) => <TetradicColorsIcon {...props} />,
+    },
+];
 
 export default function CombinationsPage(): JSX.Element {
     return (
@@ -29,15 +62,12 @@ function Title(): JSX.Element {
                     </h1>
                 </div>
                 <p className="text-lg max-w-4xl mx-auto">
-                    Tired of wasting precious hours trying to figure out the
-                    perfect shades for your vibrant brand color?
+                    Find the perfect matching shades for your primary shades
+                    with the combinations algorithm
                 </p>
                 <div className="flex flex-col sm:flex-row items-center sm:justify-center space-y-2 sm:space-y-0 sm:space-x-4 mt-6 md:mt-16">
                     <a href="#get-started" className="button primary">
-                        Get started !
-                    </a>
-                    <a href="#how-it-works" className="button">
-                        Learn more
+                        Create !
                     </a>
                 </div>
             </div>
@@ -63,7 +93,7 @@ function GetStarted(): JSX.Element {
             <div className="flex items-center">
                 <div className="container max-w-4xl rounded-3xl py-8 space-y-16">
                     <h2 className="text-center dark">Prepare your mixture</h2>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
                         <p>
                             Input your{" "}
                             <span className="text-strong dark">
@@ -94,18 +124,15 @@ function GetStarted(): JSX.Element {
                                 </button>
                             </div>
                         </form>
-                        <p className="">
+                    </div>
+                    <div className="space-y-6 flex flex-col items-center">
+                        <p>
                             Select the{" "}
                             <span className="text-strong dark">type</span> of
                             combinations that you want...
                         </p>
-                        <div className="flex flex-wrap gap-3">
-                            {[
-                                "opposite",
-                                "adjacent",
-                                "triadic",
-                                "tetradic",
-                            ].map((type) => (
+                        <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
+                            {availableCombinations.map(({ type, Icon }) => (
                                 <button
                                     onClick={() =>
                                         setType(type as CombinationType)
@@ -114,27 +141,28 @@ function GetStarted(): JSX.Element {
                                     className="button dark bg-page text-foreground capitalize"
                                     disabled={type === combinationsType}
                                 >
-                                    {type}
+                                    <Icon className="w-6 h-6" />
+                                    <span>{type}</span>
                                 </button>
                             ))}
                         </div>
-                        <div className="sm:col-span-2 flex flex-col items-center pt-8 space-y-2">
-                            <p>And take a look at your final mix</p>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                className="w-8 h-8 animate-bounce text-primary-500"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M16 10L12.7071 13.2929C12.3166 13.6834 11.6834 13.6834 11.2929 13.2929L8 10"
-                                />
-                            </svg>
-                        </div>
+                    </div>
+                    <div className="flex flex-col items-center pt-8 space-y-2">
+                        <p>And take a look at your final mix</p>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            className="w-8 h-8 animate-bounce text-primary-500"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M16 10L12.7071 13.2929C12.3166 13.6834 11.6834 13.6834 11.2929 13.2929L8 10"
+                            />
+                        </svg>
                     </div>
                 </div>
             </div>
@@ -143,9 +171,12 @@ function GetStarted(): JSX.Element {
 }
 
 function Showcase(): JSX.Element {
-    const { combinations } = useCombinations();
+    const { combinations, type } = useCombinations();
     const [inspected, inspect] = useState<Color>(null);
     const copyToClipboardRef = useRef<HTMLButtonElement>(null);
+    const currentComb = availableCombinations.find(
+        (item) => item.type === type
+    );
 
     /**
      * A very ugly way to copy and alert the user
@@ -166,7 +197,6 @@ function Showcase(): JSX.Element {
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" class="w-6 h-6 text-green-500">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 12l5 4 7-8" />
                     </svg>
-                    <span>Copied</span>
                 `;
                 setTimeout(() => {
                     target.innerHTML = html;
@@ -177,19 +207,42 @@ function Showcase(): JSX.Element {
 
     return (
         <section id="showcase" className="container">
-            <div className="space-y-16">
+            <div className="mb-24 flex flex-col items-center space-y-6 max-w-md mx-auto">
+                <currentComb.Icon className="w-16 h-16" />
+                <div>
+                    <h2 className="capitalize my-0 float-left inline-block mr-4">
+                        {type}
+                    </h2>
+                    <p className="float-none">{currentComb.description}</p>
+                </div>
+            </div>
+            <div className="space-y-24">
                 {combinations.shades.map((shades, index) => {
                     const mainColor: Color = combinations.colors[index];
                     return (
                         <div
-                            className="grid grid-cols-1 lg:grid-cols-3 items-start gap-16"
+                            className="grid grid-cols-1 lg:grid-cols-3 items-start gap-3 lg:gap-16"
                             key={index}
                         >
-                            <div className="max-w-xl mx-auto flex flex-col items-center lg:items-start">
-                                <h3 className="capitalize">{shades.name}</h3>
+                            <div className="max-w-xl flex flex-col items-center lg:items-start">
+                                <div className="flex items-center justify-between w-full">
+                                    <h3 className="capitalize mb-0 flex-1">
+                                        {shades.name}
+                                    </h3>
+
+                                    <button
+                                        ref={copyToClipboardRef}
+                                        onClick={(e) =>
+                                            copyToClipboard(e, shades)
+                                        }
+                                        className="button bg-accents hover:bg-smooth text-foreground p-3"
+                                    >
+                                        <DuplicateIcon className="w-6 h-6" />
+                                    </button>
+                                </div>
                                 <div className="w-full">
                                     <figure
-                                        className="w-3/4 h-4 rounded transition-colors duration-300"
+                                        className="w-2/3 h-4 rounded transition-colors duration-300"
                                         style={{
                                             backgroundColor: mainColor.rgb,
                                         }}
@@ -197,7 +250,7 @@ function Showcase(): JSX.Element {
                                     <p className="text-strong text-lg uppercase mb-4 mt-2 select-all">
                                         {mainColor.hex}
                                     </p>
-                                    <div className="grid grid-cols-2 gap-6">
+                                    <div className="hidden lg:grid grid-cols-2 gap-6">
                                         <div>
                                             <p className="text-smooth text-sm">
                                                 HUE
@@ -240,14 +293,6 @@ function Showcase(): JSX.Element {
                                         </div>
                                     </div>
                                 </div>
-                                <button
-                                    ref={copyToClipboardRef}
-                                    onClick={(e) => copyToClipboard(e, shades)}
-                                    className="button mt-8"
-                                >
-                                    <DuplicateIcon className="w-6 h-6" />
-                                    <span>Copy to clipboard</span>
-                                </button>
                             </div>
                             <div className="grid grid-cols-2 sm:grid-cols-5 gap-x-4 gap-y-6 lg:col-span-2 bg-background rounded-3xl p-6">
                                 {shades.colors.map((color) => (
